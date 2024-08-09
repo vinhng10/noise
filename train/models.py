7053e946bc175d4f7ef92e0010d7552d0dfb5dd6
Vinh Nguyen <vinh.nguyen@dreambroker.fi>
2024-08-09 07:56:05 +0000

    fix bugs

--- train/models.py
+++ train/models.py
@@ -1,3 +1,5 @@
+from typing import List
+import math
 import lightning.pytorch as pl
 import torch
 import torch.nn as nn
@@ -8,8 +10,16 @@ import torch.nn.functional as F
 class Model(pl.LightningModule):
     SUPPORTED_SAMPLING_RATE = 16000
 
-    def __init__(self, n_filters) -> None:
+    def __init__(
+        self,
+        n_filters: int,
+        mr_stft_lambda: float,
+        fft_sizes: List[int],
+        hop_lengths: List[int],
+        win_lengths: List[int],
+    ) -> None:
         super().__init__()
+        self.save_hyperparameters()
         self.blocks = nn.ModuleList(
             [
                 nn.Sequential(
@@ -41,13 +51,12 @@ class Model(pl.LightningModule):
         noisy_waveforms, clean_waveforms = batch
         enhanced_waveforms = self.forward(noisy_waveforms)
         l1_loss = F.l1_loss(enhanced_waveforms, clean_waveforms)
-        mrstft_loss = 0.5 * self.multi_resolution_stft_loss(
+        mrstft_loss = self.hparams.mr_stft_lambda * self.multi_resolution_stft_loss(
             enhanced_waveforms, clean_waveforms
         )
         loss = l1_loss + mrstft_loss
         self.log_dict(
             {"loss": loss, "l1_loss": l1_loss, "mrstft_loss": mrstft_loss},
-            loss,
             on_step=False,
             on_epoch=True,
             prog_bar=True,
@@ -55,15 +64,37 @@ class Model(pl.LightningModule):
         )
         return loss
 
-    def _stft(self, input, fft_size, hop_length, win_length, window):
+    def _stft_mag(self, input, fft_size, hop_length, win_length):
         x_stft = torch.stft(
-            input, fft_size, hop_length, win_length, window, return_complex=False
+            input,
+            fft_size,
+            hop_length,
+            win_length,
+            window=torch.hann_window(win_length, device=input.device),
+            return_complex=True,
         )
 
         # NOTE(kan-bayashi): clamp is needed to avoid nan or inf
-        return torch.sqrt(
-            torch.clamp(x_stft[..., 0] ** 2 + x_stft[..., 1] ** 2, min=1e-7)
-        ).transpose(2, 1)
+        return x_stft.abs().clamp(min=math.sqrt(1e-7)).transpose(2, 1)
 
-    def multi_resolution_stft_loss(self, input, target):
-        
+    def multi_resolution_stft_loss(self, x, y):
+        loss = 0
+        for fft_size, hop_length, win_length in zip(
+            self.hparams.fft_sizes,
+            self.hparams.hop_lengths,
+            self.hparams.win_lengths,
+        ):
+            x_mag = self._stft_mag(
+                x.view(-1, x.shape[2]), fft_size, hop_length, win_length
+            )
+            y_mag = self._stft_mag(
+                y.view(-1, x.shape[2]), fft_size, hop_length, win_length
+            )
+
+            # Spectral convergence loss:
+            sc_loss = torch.norm(y_mag - x_mag, p="fro") / torch.norm(y_mag, p="fro")
+            # Log STFT magnitude loss:
+            log_mag_loss = F.l1_loss(torch.log(y_mag), torch.log(x_mag))
+
+            loss += sc_loss + log_mag_loss
+        return loss
