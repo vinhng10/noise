b6f29ec33f2d3792808d90ae768622b6713e4712
Vinh Nguyen <vinh.nguyen@dreambroker.fi>
2024-08-12 11:54:49 +0000

    experiment

--- train/inference.py
+++ train/inference.py
@@ -7,17 +7,22 @@ if __name__ == "__main__":
     sampling_rate = 16000
 
     model = Model.load_from_checkpoint(
-        "./logs/model-1.0.0/checkpoints/epoch=99-train_loss=0.000.ckpt"
+        "./logs/model-1.0.5/checkpoints/epoch=479-train_loss=1.761.ckpt"
     ).eval()
     data = NoiseDataModule(
-        data_dir="./data-debug", sampling_rate=sampling_rate, batch_size=1
+        data_dir="./data-debug",
+        sampling_rate=sampling_rate,
+        batch_size=1,
+        num_workers=1,
     )
     data.prepare_data()
-    data.setup(stage="predict")
-    loader = data.val_dataloader()
+    data.setup(stage="fit")
+    loader = data.train_dataloader()
     with torch.no_grad():
         for i, (noisy_waveforms, clean_waveforms) in tqdm(enumerate(loader)):
             enhanced_waveforms = model.forward(noisy_waveforms)
             torchaudio.save(
-                f"./data-debug/{i}.wav", enhanced_waveforms.squeeze(0), sampling_rate
+                f"./data-debug/enhanced/{i}.wav",
+                enhanced_waveforms.squeeze(0),
+                sampling_rate,
             )
