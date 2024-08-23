98035adba50de936c6538cfc892eb354d8f3c14f
Vinh Nguyen <vinh.nguyen@dreambroker.fi>
2024-08-23 13:09:54 +0000

    update

--- demo/src/app/worker.js
+++ demo/src/app/worker.js
@@ -2,55 +2,44 @@ import * as ort from "onnxruntime-web/webgpu";
 
 ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
 
-// Use the Singleton pattern to enable lazy construction of the pipeline.
-class PipelineSingleton {
-  static session = null;
-  static loading = false;
-  static input = new Float32Array(4000);
+let count = 0;
+let session = null;
+let loading = false;
+let input = new Float32Array(1024);
 
-  static async getSession() {
-    if (this.session === null && !this.loading) {
-      this.loading = true;
-      const opt = {
-        executionProviders: ["webgpu"],
-      };
-      const url = new URL("./model.onnx", import.meta.url);
-      this.session = await ort.InferenceSession.create(url.toString(), opt);
-      this.loading = false;
-    }
-    return this.session;
+let getSession = async () => {
+  if (session === null && !loading) {
+    loading = true;
+    const url = new URL("./model.onnx", import.meta.url);
+    session = await ort.InferenceSession.create(url.toString(), {
+      executionProviders: ["webgpu"],
+    });
+    loading = false;
   }
+};
 
-  static getInput() {
-    return this.input;
-  }
-
-  static updateInput(newInput) {
-    const input = new Float32Array(4000);
-    const oldInput = this.input.slice(128);
-    input.set(oldInput, 0);
-    input.set(newInput, oldInput.length);
-    this.input = input;
-    return this.input;
-  }
-}
+let updateInput = (newInput) => {
+  count += 128;
+  const tmp = new Float32Array(1024);
+  const oldInput = input.slice(128);
+  tmp.set(oldInput, 0);
+  tmp.set(newInput, oldInput.length);
+  input = tmp;
+  return input;
+};
 
 self.onmessage = async (event) => {
   const { port } = event.data;
-  let session = await PipelineSingleton.getSession();
+  await getSession();
 
   // Listen for messages on the shared port
   port.onmessage = async (event) => {
-    PipelineSingleton.updateInput(event.data);
+    updateInput(event.data);
   };
 
   while (true) {
-    const tensor = new ort.Tensor(
-      "float32",
-      PipelineSingleton.getInput(),
-      [1, 1, 1, 4000]
-    );
+    const tensor = new ort.Tensor("float32", input, [1, 1, 1, 1024]);
     const result = await session.run({ input: tensor });
-    port.postMessage(result.output.data.slice(4000 - 128));
+    port.postMessage(result.output.data.slice(1024 - 128));
   }
 };
